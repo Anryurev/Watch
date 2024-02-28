c84c332cab62185a1034fda650ab31439e056a64
Anryurev <137984074+Anryurev@users.noreply.github.com>
2024-02-28 15:59:00 +0700

    Создание макета фрагмена фильма для активити Search Activity. Добавление зависимости от скалярного конвертера. Добавление конвертера в объект Retrofit

--- app/src/main/java/com/bignerdranch/android/watch/AddActivity.kt
+++ app/src/main/java/com/bignerdranch/android/watch/AddActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bignerdranch.android.watch.api.OMDbApi
 import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.create
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
-        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.omdbapi.com/").build()
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.omdbapi.com/")
+            .addConverterFactory(ScalarsConverterFactory.create()).build()
         val omdbApi: OMDbApi = retrofit.create(OMDbApi::class.java)
     }
 }
